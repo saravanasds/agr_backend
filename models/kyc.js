d38c374a5b7c7e5eb5311240ac5eab5d667fb7f1
karthikn1997 <karthikn951@gmail.com>
2024-05-02 17:27:35 +0530

    kyc updated

---
+++ models/kyc.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const kycSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  guardian: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female",],
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+  alternateMobileNumber: {
+    type: String,
+  },
+  adharno: {
+    type: String,
+    required: true,
+  },
+  adharproof: {
+    type: String,
+  },
+  photo: {
+    type: String,
+  },
+  nomineeName: {
+    type: String,
+    required: true,
+  },
+  nomineeRelationship: {
+    type: String,
+    required: true,
+  },
+  bankAcno: {
+    type: String,
+    required: true,
+  },
+  bankName: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  ifsc: {
+    type: String,
+    required: true,
+  },
+  upiId: {
+    type: String,
+    required: true,
+  },
+  paymentDate: {
+    type: Date,
+    required: true,
+  },
+  transactionId: {
+    type: String,
+    required: true,
+  },
+  paymentScreenshot: {
+    type: String,
+  },
+  referralId: {
+    type: String,
+  },
+});
+
+const Kyc = mongoose.model("Kyc", kycSchema);
+
+export { Kyc };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { authRouter } from "./auth.js";
+import { kycRouter } from "./kyc.js";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/kyc", kycRouter);
 
 export const indexRoutes = router;
