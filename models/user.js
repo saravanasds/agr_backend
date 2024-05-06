a4bdf1f875daae9694f77122ee53540adc1c0f2c
karthikn1997 <karthikn951@gmail.com>
2024-05-06 12:15:04 +0530

    test

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
@@ -21,6 +22,88 @@ const userSchema = new mongoose.Schema({
   isActive: {
     type: Boolean,
     default: false,
+  },
+  guardian: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female",],
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+  alternateMobileNumber: {
+    type: String,
+  },
+  adharno: {
+    type: String,
+    required: true,
+  },
+  adharproof: {
+    type: String,
+  },
+  photo: {
+    type: String,
+  },
+  nomineeName: {
+    type: String,
+    required: true,
+  },
+  nomineeRelationship: {
+    type: String,
+    required: true,
+  },
+  bankAcno: {
+    type: String,
+    required: true,
+  },
+  bankName: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  ifsc: {
+    type: String,
+    required: true,
+  },
+  upiId: {
+    type: String,
+    required: true,
+  },
+  paymentDate: {
+    type: Date,
+    required: true,
+  },
+  transactionId: {
+    type: String,
+    required: true,
+  },
+  paymentScreenshot: {
+    type: String,
+  },
+  referralId: {
+    type: String,
+  },
+  amount: {
+    type: Number,
+  },
+  paymentScreenshot: {
+    type: String,
   },
   randomString: String,
   randomStringExpires: Date,
