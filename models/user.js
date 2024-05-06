28928581b1d57192472ed1b5fe5691475bbf29e0
karthikn1997 <karthikn951@gmail.com>
2024-05-06 14:21:33 +0530

    updated

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
-    required: true,
+  },
+  lastName: {
+    type: String,
   },
   email: {
     type: String,
