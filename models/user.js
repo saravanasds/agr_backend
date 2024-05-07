6579e1ad2f2c0338c68681466e2a1d5401680b8b
karthikn1997 <karthikn951@gmail.com>
2024-05-07 14:39:54 +0530

    register changed

--- models/user.js
+++ models/user.js
@@ -4,9 +4,6 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
   },
-  lastName: {
-    type: String,
-  },
   email: {
     type: String,
     required: true,
@@ -17,6 +14,88 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+  },
+  guardian: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female",],
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+  alternateMobileNumber: {
+    type: String,
+  },
+  adharno: {
+    type: String,
+    required: true,
+  },
+  adharproof: {
+    type: String,
+  },
+  photo: {
+    type: String,
+  },
+  nomineeName: {
+    type: String,
+    required: true,
+  },
+  nomineeRelationship: {
+    type: String,
+    required: true,
+  },
+  bankAcno: {
+    type: String,
+    required: true,
+  },
+  bankName: {
+    type: String,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  ifsc: {
+    type: String,
+    required: true,
+  },
+  upiId: {
+    type: String,
+    required: true,
+  },
+  paymentDate: {
+    type: Date,
+    required: true,
+  },
+  transactionId: {
+    type: String,
+    required: true,
+  },
+  paymentScreenshot: {
+    type: String,
+  },
+  referralId: {
+    type: String,
+  },
+  amount: {
+    type: Number,
+  },
+  paymentScreenshot: {
+    type: String,
   },
   activationToken: {
     type: String,
