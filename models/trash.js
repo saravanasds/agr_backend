5bfd9e2c8e1851ea9e665c7404797e8a8caf88df
karthikn1997 <karthikn951@gmail.com>
2024-07-01 10:56:08 +0530

    finish

--- models/trash.js
+++ models/trash.js
@@ -8,7 +8,7 @@ const trashUserRegisterSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+      // unique: true,
       trim: true,
       lowercase: true,
     },
